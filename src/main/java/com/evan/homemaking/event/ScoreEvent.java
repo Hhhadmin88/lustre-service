f8717b8eb5db5b6b205a29470f73f5f8cf9afc2a
wangming2674 <wangmingmis@163.com>
2020-01-20 22:37:54 +0800

    refactor:add score event to optimize calculate average score

---
+++ src/main/java/com/evan/homemaking/event/ScoreEvent.java
@@ -0,0 +1,25 @@
+package com.evan.homemaking.event;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * @ClassName ScoreEvent
+ * @Description
+ * @Author EvanWang
+ * @Version 1.0.0
+ * @Date 2020/1/20 19:55
+ */
+@Slf4j
+@Getter
+public class ScoreEvent extends ApplicationEvent {
+
+    private final Integer employeeId;
+
+    public ScoreEvent(Object source, Integer employeeId) {
+        super(source);
+        this.employeeId = employeeId;
+    }
+}
