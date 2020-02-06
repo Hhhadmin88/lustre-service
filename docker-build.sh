35cbf0163c325292542326596841d1b92183d9ff
wangming2674 <wangmingmis@163.com>
2020-02-06 16:31:07 +0800

    chore: add docker login info

--- docker-build.sh
+++ docker-build.sh
@@ -4,4 +4,7 @@ mvn clean package -Dmaven.test.skip=true -U
 
 docker build -t homemaking-service .
 
-docker push registry.cn-hangzhou.aliyuncs.com/homemaking/homemaking-service[1.0.0]
+docker login --username=javawangming registry.cn-hangzhou.aliyuncs.com
+docker images
+docker tag [homemaking-service] registry.cn-hangzhou.aliyuncs.com/homemaking/homemaking-service:[1.0.0]
+docker push registry.cn-hangzhou.aliyuncs.com/homemaking/homemaking-service:[1.0.0]
