d32862874c54cc54518b3a7ac1e2afa4191f5f3a
wangming2674 <wangmingmis@163.com>
2020-02-06 22:26:29 +0800

    chore:complete docker-build.sh about password and username

--- docker-build.sh
+++ docker-build.sh
@@ -3,8 +3,6 @@
 mvn clean package -Dmaven.test.skip=true -U
 
 docker build -t homemaking-service .
-
-docker login --username=javawangming registry.cn-hangzhou.aliyuncs.com
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin registry.cn-hangzhou.aliyuncs.com
 docker images
-docker tag [homemaking-service] registry.cn-hangzhou.aliyuncs.com/homemaking/homemaking-service:[1.0.0]
 docker push registry.cn-hangzhou.aliyuncs.com/homemaking/homemaking-service:[1.0.0]
