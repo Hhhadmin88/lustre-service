e6ca895a3037b16bbf0e57b7c2f1a6a8a30ca1c4
Evan Wang <wangmingmis@163.com>
2020-02-07 13:50:04 +0800

    chore: add version env variable

--- docker-build.sh
+++ docker-build.sh
@@ -5,5 +5,5 @@ mvn clean package -Dmaven.test.skip=true -U
 docker build -t homemaking-service .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin registry.cn-hangzhou.aliyuncs.com
 docker images
-docker tag homemaking-service registry.cn-hangzhou.aliyuncs.com/homemaking/homemaking-service:1.0.0-SNAPSHOT
-docker push registry.cn-hangzhou.aliyuncs.com/homemaking/homemaking-service:1.0.0-SNAPSHOT
+docker tag homemaking-service registry.cn-hangzhou.aliyuncs.com/homemaking/homemaking-service:$VERSION
+docker push registry.cn-hangzhou.aliyuncs.com/homemaking/homemaking-service:$VERSION
